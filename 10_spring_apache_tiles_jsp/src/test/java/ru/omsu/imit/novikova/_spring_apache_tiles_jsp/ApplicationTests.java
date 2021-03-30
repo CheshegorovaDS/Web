1eec41aa3a052a8c5f595254c6019c990128c6c4
dcheshegorova <peschink@mail.ru>
2021-03-30 17:43:00 +0600

    first version

---
+++ 10_spring_apache_tiles_jsp/src/test/java/ru/omsu/imit/novikova/_spring_apache_tiles_jsp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.omsu.imit.novikova._spring_apache_tiles_jsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
